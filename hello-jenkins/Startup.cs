82001dcb46ac625e15e95c854fdb0cf81235b6a5
omer <holypuse@gmail.com>
2015-05-12 20:07:52 +0100

    test

---
+++ hello-jenkins/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hello_jenkins.Startup))]
+namespace hello_jenkins
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
